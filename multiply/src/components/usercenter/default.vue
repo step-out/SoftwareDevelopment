23c13d01ddbf61be1e1270f4d0d0e736804ca521
muer52 <95951113+muer52@users.noreply.github.com>
2024-12-31 22:46:31 +0800

    Add files via upload

---
+++ multiply/src/components/usercenter/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="waiting-container">
+        <p style="font-size:40px">服务尚未上线，敬请期待</p>
+    </div>
+</template>
+<style scoped>
+.waiting-container {
+    text-align: center;
+}
+</style>
